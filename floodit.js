7e124a3c67e19e898a543b19678a9aaf64fb96b0
Richard Smiley <rsmiley@gmail.com>
2011-02-22 02:07:49 -0500

    improvements

--- floodit.js
+++ floodit.js
@@ -1,12 +1,31 @@
-var HEIGHT = 15;
-var WIDTH = 15;
+var HEIGHT = 12;
+var WIDTH = 12;
 var COLORS = ["red","green","blue","yellow","pink","purple"];
+
+function drawMarking(x,y,c) {
+  var canvas = $("canvas")[0];
+  var ctx = canvas.getContext("2d");
+  var squareWidth = canvas.width / board.length;
+  var squareHeight = canvas.height / board[0].length;
+  ctx.fillStyle = "black";
+  ctx.fillText(c,x*squareWidth,(y+1)*squareHeight);
+}
 
 function Point(x,y) {
   this.x = x;
   this.y = y;
   this.toString = function() {
     return x+","+y;
+  }
+  this.distanceFrom = function(p2) {
+    assert(p2 instanceof Point);
+    return Math.abs(p2.x-this.x) + Math.abs(p2.y-this.y);
+  }
+  this.mark = function(c) {
+    var pt = this;
+    setTimeout(function() {
+        drawMarking(pt.x,pt.y,c);
+        }, 100);
   }
 }
 
@@ -31,6 +50,11 @@ function PointsList() {
   this.size = function() {
     return _size;
   }
+  this.mark = function(c) {
+    for (i in _list) {
+      _list[i].mark(c);
+    }
+  }
   this.list = _list;
 }
 
@@ -39,9 +63,15 @@ function Blob(points) {
   assert(points.size() > 0);
   this.points = points;
   this.size = this.points.size();
+  this.mark = this.points.mark;
   this.toString = function() {
     return "Blob (" + this.size + "): " + this.points.list;
   }
+}
+
+function BlobPoint(blob, point) {
+  this.blob = blob;
+  this.point = point;
 }
 
 var ORIGIN = new Point(0,0);
@@ -144,6 +174,9 @@ function playStrategy(strategy) {
       return;
     }
     fill(playNext);
+    if ($("input[value='oneper']").is(":checked")) {
+      return;
+    }
     ai = setTimeout(function() { playStrategy(strategy); }, 100);
   }
 }
@@ -220,7 +253,7 @@ function getEdgeCoordinates(startPoint) {
     mapper(new Point(x,y+1));
     mapper(new Point(x,y-1));
   }
-  mapper(startPoint.x,startPoint.y);
+  mapper(startPoint);
   return coords;
 }
 
@@ -228,11 +261,9 @@ function prominent() {
   var startColor = board[0][0];
   var colorProminence = newFilledArray(COLORS.length, 0);
   var edgeCoords = getEdgeCoordinates(ORIGIN);
-  for (coord in edgeCoords) {
-    var parts = coord.split(",");
-    var x = parts[0];
-    var y = parts[1];
-    colorProminence[board[x][y]] += 1;
+  for (i in edgeCoords.list) {
+    var point = edgeCoords.list[i];
+    colorProminence[board[point.x][point.y]] += 1;
   }
   return maxIndex(colorProminence);
 }
@@ -252,13 +283,13 @@ function getNumColors() {
  * finds which color gets you the longest contiguous area
  */
 function biggestBang() {
+  console.log("biggestBang");
   var startBoard = copyBoard(board);
   var area = newFilledArray(COLORS.length, 0);
-  var startPoint = new Point(0,0);
   for (i in COLORS) {
     if (i != board[0][0]) {
       flood(0,0,board[0][0],i);
-      area[i] = getFilledCoordinates(startPoint).length;
+      area[i] = getFilledCoordinates(ORIGIN).size();
     }
     board = copyBoard(startBoard);
   }
@@ -294,7 +325,7 @@ function assert(exp, message) {
   }
 }
 
-function getSizeMap() {
+function getBlobs() {
   // finds largest contigous unfilled area on board
   var filled = getFilledCoordinates(ORIGIN);
   var blobs = [];
@@ -312,35 +343,105 @@ function getSizeMap() {
   return blobs;
 }
 
-function digger() {
-  var areas = getSizeMap();
-  var parts = maxIndex(areas).split(",");
-  flash(parseInt(parts[0]),parseInt(parts[1]));
-  return -1;
-}
-
-function findLargeAreas(lowerLimit) {
-  console.log("findLargeAreas " + lowerLimit);
-  var retme = {};
-  var areas = getSizeMap();
-  for (coord in areas) {
-    if (areas[coord] >> lowerLimit) {
-      retme[coord] = true;
+function findLargeBlobs(lowerLimit) {
+  console.log("findLargeBlobs " + lowerLimit);
+  var retme = [];
+  var blobs = getBlobs();
+  for (i in blobs) {
+    var blob = blobs[i];
+    if (blob.size >= lowerLimit) {
+      blob.mark(blob.size);
+      retme.push(blob);
     }
   }
   return retme;
 }
 
+function expandFloodedByOne() {
+  var color = board[0][0];
+  var edges = getEdgeCoordinates(ORIGIN);
+  edges.mark("E");
+  for (i in edges.list) {
+    var point = edges.list[i];
+    board[point.x][point.y] = color;
+  }
+  return edges;
+}
+
+function getTouchedBlobPoint(edges, blobs) {
+  var bestBlobPoint = null;
+  for (i in edges.list) {
+    var point = edges.list[i];
+    for (j in blobs) {
+      var blob = blobs[j];
+      if (blob.points.contains(point)) {
+        if ((bestBlobPoint == null) ||
+            (blob.size > bestBlobPoint.blob.size)){
+          bestBlobPoint = new BlobPoint(blob, point);
+        }
+      }
+    }
+  }
+  return bestBlobPoint;
+}
+
+function tunnelTowards(point) {
+  assert(point instanceof Point);
+  console.log("tunnel towards " + point.toString());
+  var edges = getEdgeCoordinates(ORIGIN);
+  var shortestDistance = 100;
+  var closestPoint = null;
+  for (i in edges.list) {
+    var edgePoint = edges.list[i];
+    var distance = point.distanceFrom(edgePoint);
+    if (distance < shortestDistance) {
+      shortestDistance = distance;
+      closestPoint = edgePoint;
+    }
+  }
+  point.mark("T");
+  closestPoint.mark("C");
+  return board[closestPoint.x][closestPoint.y];
+}
+
+function largeAreas2() {
+  var b1 = copyBoard(board);
+  var largeBlobs = findLargeBlobs(3);
+  var bestBlobPoint = null;
+  var bestSize = 0;
+  // always explore to 3...discount for distance
+  for (var i=0;i<3;i++) {
+    var outerEdges = expandFloodedByOne();
+    var touchedBlobPoint = getTouchedBlobPoint(outerEdges, largeBlobs);
+    if (touchedBlobPoint != null) {
+      var size = touchedBlobPoint.blob.size - i;
+      console.log("size: " + size);
+      if (size > bestSize) {
+        bestBlobPoint = touchedBlobPoint;
+        bestSize = size;
+      }
+    }
+  }
+  board = copyBoard(b1);
+  if (bestBlobPoint != null) {
+    return tunnelTowards(bestBlobPoint.point);
+  }
+  return biggestBang();
+}
+
 function largeAreas() {
   var b1 = copyBoard(board);
-  for (coord in findLargeAreas(3)) {
-    console.log(coord);
-    var parts = coord.split(",");
-    floodWhite(parseInt(parts[0]),parseInt(parts[1]));
+  var largeBlobs = findLargeBlobs(3);
+  for (var i=0;i<3;i++) {
+    var outerEdges = expandFloodedByOne();
+    var touchedBlobPoint = getTouchedBlobPoint(outerEdges, largeBlobs);
+    if (touchedBlobPoint != null) {
+      board = copyBoard(b1);
+      return tunnelTowards(touchedBlobPoint.point);
+    }
   }
-  var b2 = copyBoard(board);
-  switchBoards(8, b1, b2);
-  return -1;
+  board = copyBoard(b1);
+  return biggestBang();
 }
 
 function switchBoards(times, orig, temp) {
@@ -356,19 +457,31 @@ function switchBoards(times, orig, temp) {
   setTimeout(function() {switchBoards(times-1, orig, temp)}, 300);
 }
 
-function floodWhite(x,y) {
-  flood(x,y,board[x][y],-1);
+function floodWhite(blob) {
+  assert(blob instanceof Blob);
+  var points = blob.points.list;
+  for (i in points) {
+    var point = points[i];
+    board[point.x][point.y] = -1;
+  }
 }
 
-function flash(x,y) {
-  var b1 = copyBoard(board);
-  floodWhite(x,y);
+function edger() {
+  var edges = getEdgeCoordinates(ORIGIN);
+  edges.mark("E");
+  return -1;
+}
+
+function expander() {
+  var moves = $("#moves");
+  moves.text(parseInt(moves.text()) + 1);
+  lastBoards.push(copyBoard(board));
+  expandFloodedByOne();
   displayBoard();
-  var b2 = copyBoard(board);
-  switchBoards(8,b1,b2);
+  return -1;
 }
 
-strategies = [inOrder, random, prominent, biggestBang, diagonal, largeAreas]
+strategies = [edger, expander, prominent, biggestBang, diagonal, largeAreas, largeAreas2]
 
 function displayStrategies() {
   var bdiv = $("#ai");
